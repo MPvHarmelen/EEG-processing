e8b3abff6b45c8f9af8bdb3d6782c93e2c5d5570
Martin van Harmelen <Martin@vanharmelen.com>
2016-12-13 16:36:27 +0100

    Fine tuned batch_function for power calculations

--- batch_function.m
+++ batch_function.m
@@ -2,7 +2,7 @@
 function batch_function(input_file, output_file)
     % ------- Choices
     % Channels to use for Fourier analysis
-    CHANNELS = [1:4];
+    CHANNELS = [1:6];
 
     % Number of time steps to include in each cut
     WINDOW_BEFORE = 52;
@@ -27,7 +27,7 @@ function batch_function(input_file, output_file)
 
     % Calculate power of data
     % data must be saved in the variable `power`, so using keyword is necessary
-    % power = data_power(data, CHANNELS);
+    power = data_power(data, CHANNELS);
 
     save(output_file, 'power');
     fprintf('Saved output to %s\n', output_file);
